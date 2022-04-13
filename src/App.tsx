d7aa61c1b5ac2381a0acc8f1f7841b11e761b7fe
b1acksmyth <smith.spence13@gmail.com>
2022-04-12 20:08:03 -0700

    wrote out logic for quiz

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+//Components Import
+import QuestionCard from './Components/QuestionCard';
 
+const TOTAL_QUESTIONS = 10;
 
-function App() {
-  return <div className="App">Hello World!</div>;
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+ 
+  const startTrivia = async () => {
+
+  }
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+  }
+  return(
+    <div className='App'>
+      <h1>React Quiz with Typescript</h1>
+      <button className='star' onClick={ startTrivia }>
+        Start
+      </button>
+      <p className='score'>Score:</p>
+      <p>Loading Questions ...</p>
+      <QuestionCard
+        questionNumber={number +1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      />
+      <button className='next' onClick={ nextQuestion }>Next</button>
+    </div>
+  );
 }
 
 export default App;
